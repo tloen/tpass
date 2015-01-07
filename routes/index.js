2dc65b45db8a0901b4c3d51286da151133ae9e14
Eric Wang <eric.w0long1@gmail.com>
2015-01-06 21:58:06 -0800

    Expanded number of fields, tried disambiguating submit buttons

--- routes/index.js
+++ routes/index.js
@@ -7,37 +7,40 @@ router.get('/', function(req, res) {
   var col=[{firstname:"fail"}];
   MongoClient.connect("mongodb://bearcatprime:196884@ds031271.mongolab.com:31271/tpassdb", function(err, db) {
     if(!err) {
-      console.log("We are connected");
+      console.log("Connection is working");
 	  var collection = db.collection('students');
 	  collection.find().toArray(function(err, items) {
 	    col = items;
 		res.render('index', { title: "Tutorial Pass, Yo" , arr: col });
 		console.log(items); });
     }
+	else console.log("Connection failed!");
   });
   console.log(col);
 });
 
 router.post('/', function(req, res) {
-  console.log(req.body)
-  var col;
-  MongoClient.connect("mongodb://bearcatprime:196884@ds031271.mongolab.com:31271/tpassdb", function(err, db) {
-    if(!err) {
-      console.log("We are connected");
-	  var collection = db.collection('students');
-	  collection.insert({
-		"firstname":req.body.firstname,
-		"lastname":req.body.lastname,
-		"homeroom":req.body.homeroom,
-		"alternate":req.body.alternate
-		"sid":req.body.sid
-		}, {w:1}, function(err, result) {});
-	  collection.find().toArray(function(err, items) {
-	    col = items;
-		res.render('index', { title: "Tutorial Pass, Yo" , arr: col });
-		console.log(items); });
-    }
-  });
+	console.log(req.body)
+	var col;
+	if("record" in req.body) {
+		MongoClient.connect("mongodb://bearcatprime:196884@ds031271.mongolab.com:31271/tpassdb", function(err, db) {
+			if(!err) {
+				console.log("We are connected");
+				var collection = db.collection('students');
+				collection.insert({
+					"firstname":req.body.firstname,
+					"lastname":req.body.lastname,
+					"homeroom":req.body.homeroom,
+					"alternate":req.body.alternate,
+					"sid":req.body.sid
+				}, {w:1}, function(err, result) {});
+				collection.find().toArray(function(err, items) {
+				col = items;
+				res.render('index', { title: "Tutorial Pass, Yo" , arr: col });
+				console.log(items); });
+			}
+		});
+	}
 });
 
 router.get('/teacher-login', function(req, res) {
