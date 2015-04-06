6195829d64742793ef2f4342fb4f6f9562950c31
Eric Wang <eric.w0long1@gmail.com>
2015-04-05 19:00:39 -0700

    re-implemented passport, to a degree

--- app.js
+++ app.js
@@ -1,27 +1,65 @@
 var express = require('express');
-var passport = require('passport');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
 
 var route = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login.js');
 
+var db = require('./db')
+ 
 var app = express();
 
+//mongoose [http://www.sitepoint.com/local-authentication-using-passport-node-js/]
+mongoose.connect(db.url);
+var Schema = mongoose.Schema;
+var UserDetail = new Schema(
+    { username: String, password: String },
+    { collection: 'users'});
+var User = mongoose.model('users', UserDetail);
+
 //passport
-app.configure(function () {
-    app.use(express.static('public'));
-    app.use(express.cookieParser());
-    app.use(express.bodyParser());
-    app.use(express.session({ secret: 'keyboard cat' }));
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+app.use(express.static('public'));
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, done) {
+    console.log('serializeUser called');
+    done(null, user);
 });
+
+passport.deserializeUser(function (id, done) {
+    /*User.findById(id, function (err, user) {
+        done(err, user);
+    });*/
+    console.log('deserializeUser called');
+    done(null, user);
+});
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+      console.log('authentication in progress');
+      User.findOne({ 'username': username }, function (err, user) {
+          if (err) { return done(err); } //server exception
+          if (!user) {
+              return done(null, false, { message: 'Incorrect username.' });
+          }
+          if (!user.validPassword(password)) {
+              return done(null, false, { message: 'Incorrect password.' });
+          }
+          return done(null, user); //passed
+      });
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,7 +69,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,42 @@
 var express = require('express');
 var router = express.Router();
-//var passport = require('passport');
 
-router.get('/',
+// okay, the following code is copied from the website 
+// http://passportjs.org/guide/username-password/
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+// okay, the following code is copied from the website 
+// http://passportjs.org/guide/username-password/
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+router.post('/login',
+    function (req, res) {
+        console.log('login request recieved from ', req.param('username'), req.param('password'));
+        debugger;
+        passport.authenticate('local', {
+            successRedirect: '/win',
+            failureRedirect: '/fail'
+        })
+    }
+);
+
+//(end of copying)
+
+router.get('/login',
     function (req, res) {
         console.log('okay');
         res.render('login');
     }
 );
 
+router.get('/win', function (req, res, next) {
+    res.send('yes');
+});
 
-console.log('hello?');
+router.get('/fail', function (req, res, next) {
+    res.send('no');
+});
 
 module.exports = router;
